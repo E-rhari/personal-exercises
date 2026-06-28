550d4027788d402268cfad3116cd419738b84b23
E-Rhari <rhari.char@gmail.com>
2026-06-28 11:10:10 -0300

    ADD: Hash code in generics
    My original idea of calculating a hash code through the variable's bits fails as soon as pointers are involved.
    However, unlike java, not all classes have a .hashCode() method (in fact, none of them do by default), but there is a library that tries to do something similar. That library is my current solution.

--- DataStructures/HashTables/ExternalChainHashTable.cpp
+++ DataStructures/HashTables/ExternalChainHashTable.cpp
@@ -4,7 +4,12 @@
 #include<stdexcept>
 #include<iostream>
 #include<cmath>
+#include <typeinfo>
+#include <type_traits>
+// #include<hash>
 using namespace std;
+
+#pragma once
 
 template <typename Type>
 class ExternalChainHashTable {
@@ -12,38 +17,41 @@ private:
     List<Type>* table;
 
     int size;
+    int amount;
 
-    // not sure if this hash is any good
-    int hash(Type x){
-        int h = 5381;
-        char* s = (char*)&x;
-        for(int i=0; i<sizeof(Type)/sizeof(char); i++)
-            h = h*33 + s[i];
-        return h;
+    
+    int hashFunc(Type x){
+        // int h = 5381;
+        // char* s = (char*)&x;
+        // for(int i=0; i<sizeof(Type)/sizeof(char); i++)
+        //     h = h*33 + s[i];
+        return hash<Type>{}(x);
     }
 
     int hashCode(Type x){
-        return hash(x) % size;
+        return abs(hashFunc(x)) % size;
     }
 
 public:
     ExternalChainHashTable(int size) {
-        this->table = (List<Type>*)malloc(sizeof(List<Type>)*size);
+        this->table = new List<Type>[size];
         for(int i=0; i<size; i++)
             this->table[i] = List<Type>();
         
         this->size = size;
+        this->amount = 0;
     }
     ExternalChainHashTable(int amountOfElements, int loadFactor) 
         : ExternalChainHashTable(ceil((float)amountOfElements/(float)loadFactor)) {}
     ~ExternalChainHashTable(){
-        free(table);
+        // do nothing
     }
 
 
     void insert(Type x){
         int index = hashCode(x);
-        table[index].insert(x);
+        if(table[index].insert(x))
+            amount++;
     }
 
 
@@ -51,16 +59,15 @@ public:
         int index = hashCode(x);
         List<Type> list = table[index];
 
-        for(int i=0; i<list.getSize(); i++)
-            if(x == list.get(i))
-                return list.getNode(i);
-        return nullptr;
+        return list.search(x)[0];
+
     }
 
 
     void remove(Type x){
         int index = hashCode(x);
-        table[index].remove(x);
+        if(table[index].remove(x))
+            amount--;
     }
 
 
@@ -74,29 +81,34 @@ public:
             table[i].print();
         }
     }
+
+
+    int getAmount(){
+        return amount;
+    }
 };
 
 
-int main(){
-    int n = 50;
+// int main(){
+//     int n = 50;
 
-    ExternalChainHashTable<int> table(n, 3);
+//     ExternalChainHashTable<int> table(n, 3);
 
-    printf("Inserting...\n");
-    for(int i=0; i<n; i++){
-        int val = -i*4 + i*i;
-        table.insert(val);
-        printf("%d,\t", val);
-    }
-    printf("\n");
-    table.print();
+//     printf("Inserting...\n");
+//     for(int i=0; i<n; i++){
+//         int val = -i*4 + i*i;
+//         table.insert(val);
+//         printf("%d,\t", val);
+//     }
+//     printf("\n");
+//     table.print();
 
-    printf("Removing...");
-    for(int i=0; i<ceil(n/2); i++){
-        int val = -i*4 + i*i;
-        table.remove(val);
-        printf("%d,\t", val);
-    }
-    printf("\n");
-    table.print();
-}
+//     printf("Removing...");
+//     for(int i=0; i<ceil(n/2); i++){
+//         int val = -i*4 + i*i;
+//         table.remove(val);
+//         printf("%d,\t", val);
+//     }
+//     printf("\n");
+//     table.print();
+// }
